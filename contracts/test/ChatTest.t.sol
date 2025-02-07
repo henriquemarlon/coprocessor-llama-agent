8c9c84ae195d1a14b046b914ddd28242351d6f9d
Henrique Marlon <henriquemarloncs@gmail.com>
2025-02-07 06:10:02 +0200

    feat: add workflow and some changes

--- contracts/test/ChatTest.t.sol
+++ contracts/test/ChatTest.t.sol
@@ -17,12 +17,13 @@ contract ChatTest is Test {
     }
 
     function test_response() public {
-        bytes memory payload = hex"6f2071756520e9206120766964613f";
+        // Payload: What is the meaning of life?
+        bytes memory payload = hex"5768617420697320746865206d65616e696e67206f66206c6966653faa";
 
-        bytes memory response =
-            hex"4120766964612c20656d20706f756361732070616c61767261732c20e920756d61206a6f726e61646120fa6e69636120666569746120646520656e636f6e74726f732c206465736166696f732c20617072656e64697a61646f732065206d6f6d656e746f7320717565206e6f73207472616e73666f726d616d2e";
+        // There isn’t a one-size-fits-all answer to the meaning of life. Whether you draw meaning from spirituality, philosophical inquiry, personal relationships, or scientific curiosity, the search itself can be a meaningful pursuit. It invites each of us to explore our values, passions, and connections, ultimately creating a purpose that is uniquely our own.
+        bytes memory output = hex"54686572652069736e2019742061206f6e652d73697a652d666974732d616c6c20616e7377657220746f20746865206d65616e696e67206f66206c6966652e205768657468657220796f752064726177206d65616e696e672066726f6d2073706972697475616c6974792c207068696c6f736f70686963616c20696e71756972792c20706572736f6e616c2072656c6174696f6e73686970732c206f7220736369656e746966696320637572696f736974792c207468652073656172636820697473656c662063616e2062652061206d65616e696e6766756c20707572737569742e20497420696e76697465732065616368206f6620757320746f206578706c6f7265206f75722076616c7565732c2070617373696f6e732c20616e6420636f6e6e656374696f6e732c20756c74696d6174656c79206372656174696e67206120707572706f7365207468617420697320756e697175656c79206f7572206f776e2e";
 
-        bytes memory notice = abi.encodeWithSignature("Notice(bytes)", response);
+        bytes memory notice = abi.encodeWithSignature("Notice(bytes)", output);
 
         bytes[] memory outputs = new bytes[](1);
         outputs[0] = notice;
@@ -33,7 +34,7 @@ contract ChatTest is Test {
         chat.runExecution(payload);
 
         vm.expectEmit();
-        emit Chat.ResultReceived(keccak256(payload), response);
+        emit Chat.ResultReceived(keccak256(payload), output);
 
         console.logBytes(outputs[0]);
 
